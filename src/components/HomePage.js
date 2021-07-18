67d93af337939a7e7cd864f71caceb18cfa70166
Rani Hinnawi <rhinnawi95@gmail.com>
2021-07-18 16:24:31 -0400

    Testing Amplify automatic deployment.

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -6,6 +6,7 @@ function HomePage(props) {
         <div className="HomePage">
             <h1>Welcome to Sunbird Web</h1>
             <p>Page under construction...</p>
+            <p>We're excited to be deploying through AWS!</p>
         </div>
     )
 }
