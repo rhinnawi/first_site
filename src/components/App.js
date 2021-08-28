d5b6680d92ea4f2efb0e4e6f5058f1d6ce9bd00e
Rani Hinnawi <rhinnawi95@gmail.com>
2021-08-28 14:35:20 -0400

    removed auth

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,13 @@ import { Route, Switch } from 'react-router';
 import HomePage from './HomePage';
 import Header from './Header';
 import Footer from './Footer';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 
 function App() {
   return (
     <div className="App">
       <header>
         <Header />
-        <h1>We now have Auth!</h1>
       </header>
-      <AmplifySignOut />
 
       <main>
         <Switch>
@@ -25,4 +22,4 @@ function App() {
   );
 }
 
-export default withAuthenticator(App);
+export default App;
