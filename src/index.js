353b3210ec3151d3d7a6816963227f206ac5930f
Rani Hinnawi <rhinnawi95@gmail.com>
2021-12-09 21:09:32 -0500

    Changed text in homepage.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './styles/index.css';
-import App from './components/App';
+import App from './App';
 import { render } from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 
